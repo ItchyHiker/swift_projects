8940b3b7e5357fc9745c8b6c42ee0cfa69b49490
ItchyHikergit <vincentcheng929@gmail.com>
2019-12-07 21:54:06 +0800

    chap09

--- ModernAutoLayout/chap09/samples/Layout-v1/Layout-v1/PreviewPanel.swift
+++ ModernAutoLayout/chap09/samples/Layout-v1/Layout-v1/PreviewPanel.swift
@@ -6,4 +6,64 @@
 //  Copyright © 2019 yuhua.cheng.ihandysoft. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class PreviewPanel: UIView {
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = spacing
+        stackView.alignment = .center
+        return stackView
+    } ()
+    var spacing: CGFloat = 16.0 {
+        didSet {
+            stackView.spacing = spacing
+        }
+    }
+    private var overflow = [UIView]()
+    
+    func show(_ items: [UIView]) {
+        stackView.arrangedSubviews.forEach {$0.removeFromSuperview()}
+        items.forEach {stackView.addArrangedSubview($0)}
+        overflow.removeAll()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        while let nextItem = overflow.first,
+            stackView.bounds.width + spacing + nextItem.intrinsicContentSize.width <= bounds.width {
+                stackView.addArrangedSubview(nextItem)
+                overflow.remove(at: 0)
+                updateConstraintsIfNeeded()
+                super.layoutSubviews()
+        }
+
+        while stackView.bounds.width > bounds.width,
+            let extraView = stackView.arrangedSubviews.last {
+                extraView.removeFromSuperview()
+                overflow.insert(extraView, at: 0)
+                updateConstraintsIfNeeded()
+                super.layoutSubviews()
+        }
+    }
+    private func setupView() {
+        addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+    
+    
+    
+}
--- ModernAutoLayout/chap09/samples/Layout-v1/Layout-v1/ViewController.swift
+++ ModernAutoLayout/chap09/samples/Layout-v1/Layout-v1/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let itemCount = 10
+    private lazy var previewPanel: PreviewPanel = {
+        let view = PreviewPanel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    } ()
+    private lazy var refreshButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Refresh", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(refresh(_:)), for: .touchUpInside)
+        return button
+    } ()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupView()
+        refreshRandomViews()
     }
+    
+    @objc private func refresh(_ sender: UIButton) {
+        refreshRandomViews()
+    }
+    private func refreshRandomViews() {
+        var views = [UIView] ()
+        for count in 0..<itemCount {
+            let view = UILabel()
+            view.text = "\(count)"
+            let size = CGFloat(arc4random_uniform(64)) + 64.0
+            view.font = UIFont.systemFont(ofSize: size)
+            view.backgroundColor = ((count % 2) == 0) ? .red : .green
+            views.append(view)
+        }
+        previewPanel.show(views)
+    }
+    private func setupView() {
+        view.backgroundColor = .yellow
+        view.addSubview(refreshButton)
+        view.addSubview(previewPanel)
+        let guide = view.layoutMarginsGuide
+        NSLayoutConstraint.activate([
+            refreshButton.centerXAnchor.constraint(equalTo: guide.centerXAnchor),
+            refreshButton.topAnchor.constraint(equalToSystemSpacingBelow: guide.topAnchor, multiplier: 1.0),
 
+            previewPanel.leadingAnchor.constraint(equalTo: guide.leadingAnchor),
+            previewPanel.centerYAnchor.constraint(equalTo: guide.centerYAnchor),
+            previewPanel.trailingAnchor.constraint(equalTo: guide.trailingAnchor)
+        ])
+    }
 
 }
 
